3942f07ca3533d405f3e4e54899453b15ea2da30
Timo Homburg <timo.homburg@hs-mainz.de>
2019-08-24 23:12:30 +0200

    Added RDF export

--- src/oghamepidoc/EpidocExtractor.java
+++ src/oghamepidoc/EpidocExtractor.java
@@ -10,6 +10,8 @@ import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.json.JSONObject;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
@@ -19,7 +21,7 @@ import org.xml.sax.ext.DefaultHandler2;
 
 public class EpidocExtractor extends DefaultHandler2 {
 
-	boolean geo=false;
+	boolean geo=false,title=false;
 	
 	GeometryFactory fac=new GeometryFactory();
 	
@@ -33,6 +35,9 @@ public class EpidocExtractor extends DefaultHandler2 {
 		case "geo": 
 			geo=true;
 			break;
+		case "title": 
+			title=true;
+			break;
 		}
 	}
 	
@@ -43,6 +48,8 @@ public class EpidocExtractor extends DefaultHandler2 {
 			String[] geospl=characters.split(",");
 			Coordinate coord=new Coordinate(Double.valueOf(geospl[0].trim()), Double.valueOf(geospl[1].trim()));
 			result.location=fac.createPoint(coord);
+		}else if(title) {
+			result.title=characters;
 		}
 	}	
 	
@@ -53,21 +60,26 @@ public class EpidocExtractor extends DefaultHandler2 {
 		switch(qName) {
 		case "geo": this.geo=false;
 			break;
+		case "title": this.title=false;
+			break;
 		}
 	}
 	
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+		OntModel model=ModelFactory.createOntologyModel();
 		List<OghamObject> resultList=new LinkedList<OghamObject>();
 		for(File folder:new File("ogham3d_epidoc_files/ogham3d_epidoc_files/").listFiles()) {
 			for(File file:folder.listFiles()) {
 				EpidocExtractor extractor=new EpidocExtractor();
 				SAXParserFactory.newInstance().newSAXParser().parse(file, extractor);
 				resultList.add(extractor.result);
+				extractor.result.toRDF(model);
 			}
 		}
 		JSONObject listresult=OghamUtils.createGeoJSON(resultList);
 		BufferedWriter writer=new BufferedWriter(new FileWriter(new File("result.json")));
 		writer.write(listresult.toString(2));
 		writer.close();
+		model.write(new FileWriter("result.ttl"), "TTL") ;
 	}
 }
--- src/oghamepidoc/OghamObject.java
+++ src/oghamepidoc/OghamObject.java
@@ -1,5 +1,12 @@
 package oghamepidoc;
 
+import java.net.URLEncoder;
+
+import org.apache.jena.ontology.DatatypeProperty;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.ObjectProperty;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.locationtech.jts.geom.Point;
@@ -10,12 +17,35 @@ public class OghamObject {
 	
 	public String name="";
 	
+	public String title="";
+	
+	public static final String BASEURI="http://www.i3mainz.de/ogham#";
+	
+	public OntModel toRDF(OntModel model) {
+		System.out.println(title);
+		OntClass oghamobj=model.createClass(BASEURI+"OghamObject");
+		OntClass geometry=model.createClass("http://www.opengis.net/ont/geosparql#Geometry");
+		OntClass spatialobject=model.createClass("http://www.opengis.net/ont/geosparql#SpatialObject");
+		OntClass feature=model.createClass("http://www.opengis.net/ont/geosparql#Feature");
+		feature.addSuperClass(spatialobject);
+		oghamobj.addSuperClass(feature);
+		geometry.addSuperClass(spatialobject);
+		Individual curind=oghamobj.createIndividual(BASEURI+URLEncoder.encode(title));
+		ObjectProperty hasgeom=model.createObjectProperty("http://www.opengis.net/ont/geosparql#hasGeometry");
+		DatatypeProperty asWKT=model.createDatatypeProperty("http://www.opengis.net/ont/geosparql#asWKT");
+		Individual geomind=geometry.createIndividual(BASEURI+URLEncoder.encode(title)+"_geom");
+		curind.addProperty(hasgeom, geomind);
+		geomind.addLiteral(asWKT, location.toText());
+    	return model;
+	}
+	
 	
 	public JSONObject toGeoJSONLD() {
 		JSONObject geojsonobj=new JSONObject();
     	geojsonobj.put("type", "Feature");
     	JSONObject properties=new JSONObject();
     	properties.put("name", name);
+    	properties.put("title", name);
     	JSONObject geom=new JSONObject();
     	JSONArray coordinates=new JSONArray();
     	coordinates.put(location.getCoordinate().x);
